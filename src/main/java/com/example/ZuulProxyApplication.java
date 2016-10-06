46a07cc7931cd81b7c00c7420b686d479a6027e5
PRD\gundave <vgundala@jhancock.com>
2016-10-06 13:59:19 -0400

    stripPrefix=false

--- src/main/java/com/example/ZuulProxyApplication.java
+++ src/main/java/com/example/ZuulProxyApplication.java
@@ -8,16 +8,39 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableCircuitBreaker
 @EnableHystrix
 @EnableHystrixDashboard
+@RestController
 public class ZuulProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulProxyApplication.class, args);
+	}
+
+	@RequestMapping({ "/user" })
+	public Map<String, Object> user(Principal principal) {
+		Map<String, Object> map = new LinkedHashMap<>();
+		map.put("name", principal.getName());
+		OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) principal;
+		map.put("credentials", oAuth2Authentication.getCredentials());
+		map.put("Auth2Request", oAuth2Authentication.getOAuth2Request());
+		map.put("UserAuthentication", oAuth2Authentication.getUserAuthentication());
+		map.put("Authorities", oAuth2Authentication.getAuthorities());
+/*		map.put("Details", oAuth2Authentication.getDetails());
+		map.put("Class", oAuth2Authentication.getClass());*/
+
+		return map;
 	}
 
 	@Bean
